ec95370950a26ecf08bf2f516ef30e941ce4a4df
Casey Sobecks <csobe126@gmail.com>
2024-06-22 23:38:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const player = new Player.Player(client);
 client.on("error",console.error);
 client.on("warn",console.warn);
 
-player.extractors.loadDefault((ext));
+player.extractors.loadDefault();
 
 client.on("messageCreate", deployment);
 
