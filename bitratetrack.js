46ba28dfe10d575e4fcb52694c3c9129ee1a39e4
Casey Sobecks <csobe126@gmail.com>
2024-06-22 23:26:10 -0400

    Update bitratetrack.js

--- bitratetrack.js
+++ bitratetrack.js
@@ -1,8 +1,8 @@
 import useQueue from "discord-player"
 
-export async function bitrate(interaction){
+export async function bitrate(interaction) {
     const queue = useQueue(interaction.guildId);
     const value=interaction.options.getInteger('bitrate');
     queue.node.setBitrate(value);
-    return void interaction.followUp ({content: `bitrate set to ${value} bits `});
+    return interaction.followUp ({content: `bitrate set to ${value} bits `});
 }
