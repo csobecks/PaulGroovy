c275e68274ea21cfa11bb181f3d234f13bc589aa
Casey Sobecks <csobe126@gmail.com>
2024-06-22 23:14:47 -0400

    update to point to files

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import interactionHandler from "./interaction"
-import deployment from "./deploy"
+import interactionHandler from "../interaction.js"
+import deployment from "../deploy.js"
 
 import  Client from "discord.js"
 import  Player from "discord-player"
-import config from "./config.json"
+import config from "../config.json"
 
 const player = new Player(readyClient);
 
