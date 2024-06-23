f5146588b0607650789bd53208b4a0fc5817e9f1
Casey Sobecks <csobe126@gmail.com>
2024-06-22 23:48:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const player = new Player.Player(client);
 client.on("error",console.error);
 client.on("warn",console.warn);
 
-player.extractors.loadDefault();
+player.extractors.loadDefault(('YouTubeExtractor'));
 
 client.on("messageCreate", deployment);
 
