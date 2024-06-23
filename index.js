e4b5236849f5c7188acd7f13100e2fb0bc241b82
Casey Sobecks <csobe126@gmail.com>
2024-06-23 19:57:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { interactionHandler } from "./interaction.js"
 import { deployment } from "./deploy.js"
+import { useQueue } from "discord-player"
 
 import  Discord from "discord.js"
 import  Player from "discord-player"
@@ -41,56 +42,63 @@ client.on("interactionCreate", interactionHandler);
 
 
 
-// player.events.on('playerStart', (queue, track) => {
-//     // Emitted when the player starts to play a song
-//     queue.metadata.send(`Started playing: **${track.title}**`);
-// });
+player.events.on('playerStart', (interaction, track) => {
+    // Emitted when the player starts to play a song
+    const queue = useQueue(interaction.guild.id);
+    queue.metadata.send(`Started playing: **${track.title}**`);
+});
 
-// player.events.on('audioTrackAdd', (queue, track) => {
-//     // Emitted when the player adds a single song to its queue
-//     queue.metadata.send(`Track **${track.title}** queued`);
-// });
+player.events.on('audioTrackAdd', (interaction, track) => {
+    // Emitted when the player adds a single song to its queue
+    const queue = useQueue(interaction.guild.id);
+    queue.metadata.send(`Track **${track.title}** queued`);
+});
 
-// player.events.on('audioTracksAdd', (queue, track) => {
-//     // Emitted when the player adds multiple songs to its queue
-//     queue.metadata.send(`Multiple Track's queued`);
-// });
+player.events.on('audioTracksAdd', (interaction) => {
+    // Emitted when the player adds multiple songs to its queue
+    const queue = useQueue(interaction.guild.id);
+    queue.metadata.send(`Multiple Track's queued`);
+});
 
-// player.events.on('playerSkip', (queue, track) => {
-//     // Emitted when the audio player fails to load the stream for a song
-//     queue.metadata.send(`Skipping **${track.title}**`);
-// });
+player.events.on('playerSkip', (interaction, track) => {
+    // Emitted when the audio player fails to load the stream for a song
+    const queue = useQueue(interaction.guild.id);
+    queue.metadata.send(`Skipping **${track.title}**`);
+});
 
-// player.events.on('disconnect', (queue) => {
-//     // Emitted when the bot leaves the voice channel
-//     queue.metadata.send('leaving now');
-// });
+player.events.on('disconnect', (interaction) => {
+    // Emitted when the bot leaves the voice channel
+    const queue = useQueue(interaction.guild.id);
+    queue.metadata.send('leaving now');
+});
 
-// player.events.on('emptyChannel', (queue) => {
-//     // Emitted when the voice channel has been empty for the set threshold
-//     // Bot will automatically leave the voice channel with this event
-//     queue.metadata.send(`Leaving because no vc activity for the past 5 minutes`);
-// });
+player.events.on('emptyChannel', (interaction) => {
+    // Emitted when the voice channel has been empty for the set threshold
+    // Bot will automatically leave the voice channel with this event
+    const queue = useQueue(interaction.guild.id);
+    queue.metadata.send(`Leaving because no vc activity for the past 5 minutes`);
+});
 
-// player.events.on('emptyQueue', (queue) => {
-//     // Emitted when the player queue has finished
-//     queue.metadata.send('Queue finished!');
-// });
-player.events.on('error', (queue, error) => {
+player.events.on('emptyQueue', (interaction) => {
+    // Emitted when the player queue has finished
+    const queue = useQueue(interaction.guild.id);
+    queue.metadata.send('Queue finished!');
+});
+player.events.on('error', (error) => {
     // Emitted when the player queue encounters error
     console.log(`General player error event: ${error.message}`);
     console.log(error);
 });
 
-player.events.on('playerError', (queue, error) => {
+player.events.on('playerError', (error) => {
     // Emitted when the audio player errors while streaming audio track
     console.log(`Player error event: ${error.message}`);
     console.log(error);
 });
 
-// player.on('debug', async(message) =>{
-//     console.log(`General player debug event: ${message}`);
-// });
-player.events.on('debug', async(queue,message) => {
+player.on('debug', async(message) =>{
+    console.log(`General player debug event: ${message}`);
+});
+player.events.on('debug', async(message) => {
     console.log(`Player debug event: ${message}`);
 });
