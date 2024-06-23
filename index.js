c04f03083825f0e830958e71db4e5e2d8c5ef35e
Casey Sobecks <csobe126@gmail.com>
2024-06-22 23:52:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -40,41 +40,41 @@ client.on("interactionCreate", interactionHandler);
 
 
 
-player.events.on('playerStart', (queue, track) => {
-    // Emitted when the player starts to play a song
-    queue.metadata.send(`Started playing: **${track.title}**`);
-});
+// player.events.on('playerStart', (queue, track) => {
+//     // Emitted when the player starts to play a song
+//     queue.metadata.send(`Started playing: **${track.title}**`);
+// });
 
-player.events.on('audioTrackAdd', (queue, track) => {
-    // Emitted when the player adds a single song to its queue
-    queue.metadata.send(`Track **${track.title}** queued`);
-});
+// player.events.on('audioTrackAdd', (queue, track) => {
+//     // Emitted when the player adds a single song to its queue
+//     queue.metadata.send(`Track **${track.title}** queued`);
+// });
 
-player.events.on('audioTracksAdd', (queue, track) => {
-    // Emitted when the player adds multiple songs to its queue
-    queue.metadata.send(`Multiple Track's queued`);
-});
+// player.events.on('audioTracksAdd', (queue, track) => {
+//     // Emitted when the player adds multiple songs to its queue
+//     queue.metadata.send(`Multiple Track's queued`);
+// });
 
-player.events.on('playerSkip', (queue, track) => {
-    // Emitted when the audio player fails to load the stream for a song
-    queue.metadata.send(`Skipping **${track.title}**`);
-});
+// player.events.on('playerSkip', (queue, track) => {
+//     // Emitted when the audio player fails to load the stream for a song
+//     queue.metadata.send(`Skipping **${track.title}**`);
+// });
 
-player.events.on('disconnect', (queue) => {
-    // Emitted when the bot leaves the voice channel
-    queue.metadata.send('leaving now');
-});
+// player.events.on('disconnect', (queue) => {
+//     // Emitted when the bot leaves the voice channel
+//     queue.metadata.send('leaving now');
+// });
 
-player.events.on('emptyChannel', (queue) => {
-    // Emitted when the voice channel has been empty for the set threshold
-    // Bot will automatically leave the voice channel with this event
-    queue.metadata.send(`Leaving because no vc activity for the past 5 minutes`);
-});
+// player.events.on('emptyChannel', (queue) => {
+//     // Emitted when the voice channel has been empty for the set threshold
+//     // Bot will automatically leave the voice channel with this event
+//     queue.metadata.send(`Leaving because no vc activity for the past 5 minutes`);
+// });
 
-player.events.on('emptyQueue', (queue) => {
-    // Emitted when the player queue has finished
-    queue.metadata.send('Queue finished!');
-});
+// player.events.on('emptyQueue', (queue) => {
+//     // Emitted when the player queue has finished
+//     queue.metadata.send('Queue finished!');
+// });
 player.events.on('error', (queue, error) => {
     // Emitted when the player queue encounters error
     console.log(`General player error event: ${error.message}`);
