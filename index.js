88bdb4a908351be2482d9d5c54c529a6a28b365d
Casey Sobecks <csobe126@gmail.com>
2024-06-23 20:00:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -45,44 +45,44 @@ client.on("interactionCreate", interactionHandler);
 player.events.on('playerStart', (interaction, track) => {
     // Emitted when the player starts to play a song
     const queue = useQueue(interaction.guild.id);
-    queue.metadata.send(`Started playing: **${track.title}**`);
+    interaction.followUp({content:`Started playing: **${track.title}**`});
 });
 
 player.events.on('audioTrackAdd', (interaction, track) => {
     // Emitted when the player adds a single song to its queue
     const queue = useQueue(interaction.guild.id);
-    queue.metadata.send(`Track **${track.title}** queued`);
+    interaction.followUp({content:`Track **${track.title}** queued`});
 });
 
 player.events.on('audioTracksAdd', (interaction) => {
     // Emitted when the player adds multiple songs to its queue
     const queue = useQueue(interaction.guild.id);
-    queue.metadata.send(`Multiple Track's queued`);
+    interaction.followUp({content:`Multiple Track's queued`});
 });
 
 player.events.on('playerSkip', (interaction, track) => {
     // Emitted when the audio player fails to load the stream for a song
     const queue = useQueue(interaction.guild.id);
-    queue.metadata.send(`Skipping **${track.title}**`);
+    interaction.followUp({content:`Skipping **${track.title}**`});
 });
 
 player.events.on('disconnect', (interaction) => {
     // Emitted when the bot leaves the voice channel
     const queue = useQueue(interaction.guild.id);
-    queue.metadata.send('leaving now');
+    interaction.followUp({content:'leaving now'});
 });
 
 player.events.on('emptyChannel', (interaction) => {
     // Emitted when the voice channel has been empty for the set threshold
     // Bot will automatically leave the voice channel with this event
     const queue = useQueue(interaction.guild.id);
-    queue.metadata.send(`Leaving because no vc activity for the past 5 minutes`);
+    interaction.followUp({content:`Leaving because no vc activity for the past 5 minutes`});
 });
 
 player.events.on('emptyQueue', (interaction) => {
     // Emitted when the player queue has finished
     const queue = useQueue(interaction.guild.id);
-    queue.metadata.send('Queue finished!');
+    interaction.followUp({content:'Queue finished!'});
 });
 player.events.on('error', (error) => {
     // Emitted when the player queue encounters error
