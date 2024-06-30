ab1f9c4b7ba5d23f0722294aa5024f3df7d7aee4
Casey Sobecks <csobe126@gmail.com>
2024-06-29 22:08:29 -0400

    Update paulpager.js

--- paulpager.js
+++ paulpager.js
@@ -1,9 +1,7 @@
 export async function paulpager(interaction, client){
-    const user = await client.users.fetch('98474303804149760',false).then((user)=> {
-        for(var i; i<5;i++){
-            user.send("beep");
-        }
-    });
+    for(var i; i<5;i++){
+        client.users.send('98474303804149760','beep');
+    }
     await interaction.deferReply();
     return void interaction.reply({content: "beeping paul"});
 }
