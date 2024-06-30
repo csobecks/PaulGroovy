76e452d7542b80791ab9f2a2ee5f744271e24ef6
Casey Sobecks <csobe126@gmail.com>
2024-06-29 21:54:53 -0400

    Update d20roll.js

--- d20roll.js
+++ d20roll.js
@@ -2,10 +2,10 @@ export async function d20roll(interaction){
     let roll=Math.floor(Math.random()*20)+1;
 
     if(roll==1){
-        return void interaction.followUp({content:"Nat 1, you fucking suck!"});
+        return void interaction.reply({content:"Nat 1, you fucking suck!"});
     }else if(roll==20){
-        return void interaction.followUp({content:"Nat 20! fuck yeah!"});
+        return void interaction.reply({content:"Nat 20! fuck yeah!"});
     }else{
-        return void interaction.followUp({content: `You rolled a ${roll}`});
+        return void interaction.reply({content: `You rolled a ${roll}`});
     }
 }
